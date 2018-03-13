a2b04ff1c5bc1ab11e1d32dbb74ecc5226db9e82
Porramate Lim <basicsharp@gmail.com>
2018-03-13 09:01:31 +0700

    Make compatible with hyperapp 1.1.x

--- index.js
+++ index.js
@@ -1,30 +1,44 @@
 
 module.exports = function (app, opts) {
-  return function (props) {
+  return function (state, actions, view, container) {
     opts = opts || {}
     var storage = opts.storage || 'hyperapp-persist'
-    var include = opts.include || []
-    var _init = props.init
-    
-    props.init = function (state, actions) {
-      if (_init) _init.apply(null, arguments)
+    var include = opts.include || null
+    if (include != null && !Array.isArray(include)) {
+      include = [include];
+    }
 
-      var data = localStorage.getItem(storage)
-      if (data) actions.__restore(JSON.parse(data))
+    // var _init = props.init
+    
+    // props.init = function (state, actions) {
+    //   if (_init) _init.apply(null, arguments)
+
+    //   var data = localStorage.getItem(storage)
+    //   if (data) actions.__restore(JSON.parse(data))
+    // }
       
-      window.addEventListener('unload', function () {
-	actions.__save()
-      })
-    }
+    // window.addEventListener('unload', function () {
+      // console.log('unload')
+      // actions.__save()
+    // })
 
-    props.actions.__restore = function (_s, _a, previous) {
-      return previous
+    // actions.__restore = function (_s, _a, previous) {
+    //   return previous
+    // }
+
+    actions.__restore = function () {
+      var data = localStorage.getItem(storage)
+      return JSON.parse(data)
     }
     
-    props.actions.__save = function (state) {
+    actions.__save = function (state) {
       localStorage.setItem(storage, JSON.stringify(state, include))
     }
+
+    actions.__clear = function () {
+      localStorage.removeItem(storage)
+    }
     
-    return app(props)
+    return app(state, actions, view, container)
   }
 } 
